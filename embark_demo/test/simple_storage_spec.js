d13d27ef37ec194f5c328e30067bd51fd8d7211a
Jefwillems <jefwillems100@gmail.com>
2017-06-21 10:51:49 +0200

    test

--- embark_demo/test/simple_storage_spec.js
+++ embark_demo/test/simple_storage_spec.js
@@ -3,8 +3,8 @@ var Embark = require('embark');
 var EmbarkSpec = Embark.initTests();
 var web3 = EmbarkSpec.web3;
 
-describe("SimpleStorage", function() {
-  before(function(done) {
+describe("SimpleStorage", function () {
+  before(function (done) {
     var contractsConfig = {
       "SimpleStorage": {
         args: [100]
@@ -13,16 +13,17 @@ describe("SimpleStorage", function() {
     EmbarkSpec.deployAll(contractsConfig, done);
   });
 
-  it("should set constructor value", function(done) {
-    SimpleStorage.storedData(function(err, result) {
+  it("should set constructor value", function (done) {
+    SimpleStorage.storedData(function (err, result) {
+
       assert.equal(result.toNumber(), 100);
       done();
     });
   });
 
-  it("set storage value", function(done) {
-    SimpleStorage.set(150, function() {
-      SimpleStorage.get(function(err, result) {
+  it("set storage value", function (done) {
+    SimpleStorage.set(150, function () {
+      SimpleStorage.get(function (err, result) {
         assert.equal(result.toNumber(), 150);
         done();
       });
