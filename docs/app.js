27ce4194136452abeea647931424ca3138e6b080
wimdetr <wim.detroyer.w3755@student.hogent.be>
2017-06-27 14:17:34 +0200

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -8,6 +8,7 @@ window.addEventListener('load', function() {
     if (typeof web3 !== 'undefined') {
         // Use Mist/MetaMask's provider
         window.web3 = new Web3(web3.currentProvider);
+        console.log("it works!");
     } else {
         console.log('No web3? You should consider trying MetaMask!')
         // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
