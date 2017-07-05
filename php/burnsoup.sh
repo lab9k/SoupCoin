511f3c5a440713f324b1f86baef0209b298f2c82
Jefwillems <jefwillems100@gmail.com>
2017-07-05 17:03:47 +0200

    added server side scripts

---
+++ php/burnsoup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function runTransaction {
+	geth attach --exec "loadScript(\"soupcoinburnscript.js\"); burnSoup(\"$1\")";
+}
+ 
+function startGeth {
+          $(geth --networkid=4 --datadir=$HOME/.rinkeby --cache=512 --ethstats='''yournode:Respect my authoritah!@stats.rinkeby.io''' --bootnodes=enode://a24ac7c5484ef4ed0c5eb2d36620ba4e4aa13b8c84684e1b4aab0cebea2ae45cb4d375b77eab5616d34bfbd3c1a833fc51296ff084b770b94fb9028c4d25ccf@52.169.42.101:30303 --rpc --rpcapi="personal,eth,network" &>/dev/null &disown )
+ }
+
+if [ $(ps | grep geth | wc -l) -ge 1 ]
+ 19 
+ 20         then
+ 21 
+ 22         echo "Geth is running, attaching now!"
+ 23 
+ 24 else
+ 25 
+ 26         echo "Geth not running, starting now:"
+ 27 
+ 28         startGeth
+ 29 
+ 30         echo "Geth is running now"
+ 31 
+ 32         sleep 10
+ 33 
+ 34         echo "Running transaction"
+ 35 
+ 36 fi
+
+runTransaction $1;
+
+exit 0;
