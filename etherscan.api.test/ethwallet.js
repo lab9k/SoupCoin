442e650d7c8a58a7c919f6f6d5793ac950c27e5d
Jefwillems <jefwillems100@gmail.com>
2017-07-07 14:44:09 +0200

    etherscan api nu zonder jquery

--- etherscan.api.test/ethwallet.js
+++ etherscan.api.test/ethwallet.js
@@ -1,29 +1,34 @@
 const contractAddress = "0x19B3065F1b82f2c8f8DA8ed4577abB1E448eCEf3";
 const apiKey = "JTI5CD7MSAIHYI53FXFXW7B9JXVJ15E36H";
+const url = `https://api.etherscan.io/api?module=contract&action=getabi&address=${contractAddress}&apikey=${apiKey}`;
+console.log(url);
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider());
 const version = web3.version.api;
-/*https://api.etherscan.io/api?module=contract&action=getabi&address=0xBB9bc244D798123fDe783fCc1C72d3Bb8C189413&apikey=YourApiKeyToken*/
-$.getJSON(`https://api.etherscan.io/api?module=contract&action=getabi&address=${contractAddress}&apikey=${apiKey}`, function (data) {
-    let contractABI = "";
-    contractABI = JSON.parse(data.result);
-    if (contractABI !== '') {
-        const MyContract = web3.eth.contract(contractABI);
-        const myContractInstance = MyContract.at(contractAddress);
-        console.log("contractInstance: ", myContractInstance);
 
-        myContractInstance.owner(function (error, owner) {
-            console.log("owner error: ", error);
-            console.log("owner value: ", owner);
-            console.log("\n");
-        });
-
-        myContractInstance.symbol(function (error, symbol) {
-            console.log("symbol error: ", error);
-            console.log("symbol value: ", symbol);
-            console.log("\n");
-        });
+const getJSON = function (url, callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+        let status = xhr.status;
+        if (status === 200) {
+            callback(null, xhr.responseText);
+        } else {
+            callback(status);
+        }
+    };
+    xhr.send();
+};
+getJSON(url, function (err, data) {
+    if (err !== null) {
+        console.log('Something went wrong: ' + err);
     } else {
-        console.log("Error");
+        //console.log(data, "\ntypeof data: ", typeof data);
+        let json = JSON.parse(data).result;
+        let jsonResult = JSON.parse(json);
+        const myContract = web3.eth.contract(jsonResult);
+        const myContractInstance = myContract.at(contractAddress);
+        console.log(myContractInstance);
     }
 });
