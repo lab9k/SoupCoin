47ddc703b6b008a2203d4edcc3c1fa0bdf09b296
Jefwillems <jefwillems100@gmail.com>
2017-07-10 12:39:20 +0200

    code cleanup

--- etherscan.api.test/sendRawTransaction.js
+++ etherscan.api.test/sendRawTransaction.js
@@ -1,8 +1,22 @@
-const config = require('./sendRawConfig.json');
+const configuration = require('./sendRawConfig.json');
 
+//change this when switching net
+const config = configuration.rinkeby;
+
+const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.HttpProvider(config.httpProvider));
+const version = web3.version.api;
 
 const rawTransaction = {
-    buildRawTransaction: function () {
+    buildCallback: function (err, data) {
+        if (err) {
+            console.log('Something went wrong: ' + err);
+        } else {
+            let json = JSON.parse(data).result;
+            let jsonResult = JSON.parse(json);
+            this.contract = web3.eth.contract(jsonResult);
+            this.contractInstance = this.contract.at(config.contractAddress);
+        }
 
     },
     getNonce: function () {
@@ -22,5 +36,27 @@ const rawTransaction = {
     },
     getChainId: function () {
 
+    },
+    buildTransaction: function (url) {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType = 'json';
+        xhr.onload = function () {
+            let status = xhr.status;
+            if (status === 200) {
+                this.buildCallback(null, xhr.responseText);
+            } else {
+                this.buildCallback(status);
+            }
+        };
+        xhr.send();
     }
 };
+
+const url = config.apiBaseUrl +
+    "module=contract&action=getabi&address=" +
+    config.contractAddress +
+    "&apikey=" +
+    config.apiKey;
+
+rawTransaction.buildTransaction(url);
