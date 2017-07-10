84b2125f2f841cdc0e452e966a110cabb460694c
Jefwillems <jefwillems100@gmail.com>
2017-07-10 12:00:34 +0200

    added config for easier switching to mainnet-rinkeby

---
+++ etherscan.api.test/sendRawTransaction.js
@@ -0,0 +1,26 @@
+const config = require('./sendRawConfig.json');
+
+
+const rawTransaction = {
+    buildRawTransaction: function () {
+
+    },
+    getNonce: function () {
+
+    },
+    getGasPrice: function () {
+
+    },
+    getGasLimit: function () {
+
+    },
+    getValue: function () {
+
+    },
+    getData: function () {
+
+    },
+    getChainId: function () {
+
+    }
+};
